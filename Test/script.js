47e44618a95de3f963f374633e6d23a1373708c0
Leonid <leonidbars1993@gmail.com>
2019-04-09 13:10:31 +0300

    Это наш первый комит

---
+++ Test/script.js
@@ -0,0 +1,36 @@
+'use stcict';
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null; // чего то в коде не существует 
+undefined; // какойто объект существует а значения не имеет
+var obj = {};
+
+console.log(4/0); 
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name : "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+   let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+   console.log(arr[0]);
+
+  // alert('Hello World');
+  //let answer =  confirm('Are you here');
+  //console.log(answer);
+
+  let answer =  prompt('Есть ли вам 18','Да');
+  console.log(typeof(null));
+
+
